e3a48d230f475669215af340f5c79c7fe848bc73
Alexis <alexis.prieto@tecsup.edu.pe>
2026-09-22 01:04:10 -0500

    Configurar rutas de navegación con la clase Screen

---
+++ Semana05/app/src/main/java/com/prieto/navlab/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.prieto.navlab.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen(route = "home")
+    object List : Screen(route = "list")
+    object Profile : Screen(route = "profile")
+    object Detail : Screen(route = "detail/{itemId}") {
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
+}
