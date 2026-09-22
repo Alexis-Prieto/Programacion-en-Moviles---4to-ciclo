5910b150f634cbdb0364741eba491828ca21bb9c
Alexis <alexis.prieto@tecsup.edu.pe>
2026-09-22 02:09:17 -0500

    Corregir importación faltante de ListScreen en Appnavigation

--- Semana05/app/src/main/java/com/prieto/navlab/navigation/AppNavigation.kt
+++ Semana05/app/src/main/java/com/prieto/navlab/navigation/AppNavigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.prieto.navlab.screens.ListScreen
 import com.prieto.navlab.screens.DetailScreen
 import com.prieto.navlab.screens.HomeScreen
 import com.prieto.navlab.screens.ProfileScreen
