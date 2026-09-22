259a8b415fc1a31c210a2d154098738219f04240
Alexis <alexis.prieto@tecsup.edu.pe>
2026-09-22 01:19:44 -0500

    Conectar NavHost en MainActivity mediante AppNavigation

--- Semana05/app/src/main/java/com/prieto/navlab/MainActivity.kt
+++ Semana05/app/src/main/java/com/prieto/navlab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.prieto.navlab.navigation.AppNavigation
 import com.prieto.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,16 +22,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavLabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        AppNavigation()
+                    }
                 }
             }
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -37,7 +37,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
---
+++ Semana05/app/src/main/java/com/prieto/navlab/navigation/AppNavigation.kt
@@ -0,0 +1,44 @@
+package com.prieto.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.prieto.navlab.screens.DetailScreen
+import com.prieto.navlab.screens.HomeScreen
+import com.prieto.navlab.screens.ListScreen
+import com.prieto.navlab.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+        composable(route = Screen.List.route) {
+            ListScreen(navController = navController)
+        }
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(navController = navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController = navController, itemId = itemId)
+        }
+    }
+}
